2ee6bf5095f6cdbd7a496bdeda137634eaa7f463
zhiqiang37 <zhiqiang37@staff.weibo.com>
2019-07-25 19:59:03 +0800

    加入for循环，回车控制继续播放

--- repic.py
+++ repic.py
@@ -1,38 +1,49 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mimg
 import numpy as np
+import  time
 import cv2
 #path_pic=input("请输入图片")
-# path:文件地址 num：输入文件的个数 list_source：图片通用名，列表
+# path:文件地址 num：图片的帧数 list_source：图片通用名，列表
 
 def  showdiff(path, num, list_source):
-    num_source=len(list_source)
+    tpye_source=len(list_source) #对比试验个数
     imglist=[]
-
+    title=[]
+    title="test"
     for i in range(1, num):
-        for j in num_source:
-            imglist[j]=cv2.imread(path+list_source[j]+str(i)+'.jpeg')
-            title=list_source[j]
+        #cv2.namedWindow(title)
+        for j in range(0,tpye_source):
+            in_path=path+list_source[j]+'-00'+str(i)+'.jpeg'
+            imglist=cv2.imread(in_path) #读入图片
+            fontInPic=list_source[j]                                   #初始化所展示图片所属title
+            #cv2.namedWindow(title, cv2.WINDOW_FULLSCREEN)
 
-        cv2.namedWindow(title,cv2.WINDOW_FULLSCREEN)
+            #cv2.setWindowProperty(title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            cv2.putText(imglist, fontInPic+'-'+str(i), (100, 100), cv2.FONT_HERSHEY_DUPLEX, 3, (13, 46, 89), 3, 8)
+            cv2.imshow(title, imglist)
+            cv2.waitKey(10)
+            Comd=input('Please decide stop?')
+            if Comd!='\r':
+                time.sleep(1)
 
-        img_con1 = cv2.imread(path + 'rec_crf23seq4-00' + str(i) + '.jpeg')
-        img_con2 = cv2.imread(path + 'rec_crf25seq4-00' + str(i) + '.jpeg')
-        title_1 = 'crf23-pic' + str(i)
-        title_2 = 'crf25-pic' + str(i)
-        cv2.namedWindow(title_1, cv2.WINDOW_FULLSCREEN)
-        # cv2.namedWindow("测试图像",cv2.WINDOW_FULLSCREEN)
-        cv2.setWindowProperty(title_1, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        cv2.putText(img_con1, "crf23", (100, 100), cv2.FONT_HERSHEY_DUPLEX, 3, (13, 46, 89), 3, 8)
-        cv2.imshow(title_1, img_con1)
 
-        cv2.waitKey(2000)
-        # cv2.namedWindow(title_1, cv2.WINDOW_FULLSCREEN)
-        # cv2.setWindowProperty(title_2, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        cv2.putText(img_con2, "crf25", (100, 100), cv2.FONT_HERSHEY_DUPLEX, 3, (13, 46, 89), 3, 8)
-        cv2.imshow(title_1, img_con2)
-        # cv2.imshow("测试图像",img_con2)
-        cv2.waitKey(2000)
+
+
+
+
+
+
+
+path='/Users/zhiqiang37/Documents/vbv/seq11_高触发/'
+num= 8
+list_source=['rec_crf23_seq11','rec_crf25_seq11']
+showdiff(path,num,list_source)
+
+
+
+
+
 
 
 
